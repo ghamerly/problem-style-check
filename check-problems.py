711bb59a57ea14d669beb90c75a852b074545578
Greg Hamerly <hamerly@cs.baylor.edu>
2022-05-12 11:57:58 -0500

    make the script more robust to running in different environments
    - load 3rd-party modules conditionally; don't die
    - allow specifying the cache of problem names on the command line
    - change the type of format string used

--- check-problems.py
+++ check-problems.py
@@ -44,16 +44,33 @@ import datetime
 import glob
 import io
 import os.path
+import pathlib
 import re
 import subprocess
+import sys
 import traceback
 import yaml
 
-import plasTeX.TeX
-import plasTeX.Logging
+def try_importing(module):
+    '''Load the non-standard modules that we can load into the global namespace,
+    without dying.'''
 
-import kattis.util.db
-import problemtools.verifyproblem
+    # if the module has multiple parts, load each separately
+    parts = module.split('.')
+    for i in range(len(parts)):
+        name = '.'.join(parts[:i+1])
+        try:
+            globals()[name] = __import__(name)
+        except ModuleNotFoundError:
+            print(f'could not import module {name} (while loading {module})')
+            # if we cannot load a parent module, we won't be able to load the
+            # children either
+            break
+
+try_importing('kattis.util.db')
+try_importing('plasTeX.TeX')
+try_importing('plasTeX.Logging')
+try_importing('problemtools.verifyproblem')
 
 # globals, yuck
 _SPELLING_DICTIONARIES = {}
@@ -71,11 +88,37 @@ def _log(dest, key, message):
 warning = lambda key, message: _log(_ERRORS, key, message)
 error = lambda key, message: _log(_ERRORS, key, message)
 
-def display_warnings_errors():
-    p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=open(os.devnull, 'w'))
-    git_hash = p.communicate(None)[0].strip().decode('utf-8')
+def _check_problem_name_uniqueness(problems, cache_filename=None):
+    """Check that all the names are not already used in Kattis."""
 
-    logfilename = datetime.datetime.now().strftime('problem-check-log-%Y%m%d-%H%M%S{}{}.txt'.format('-' if git_hash else '', git_hash))
+    kattis_names = None
+
+    if 'kattis.util.db' in sys.modules:
+        # TODO: don't depend on the Kattis database; use a REST endpoint
+        with kattis.util.db.db_admin_txn() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT problem_name FROM problem')
+            kattis_names = {p for [p] in cursor}
+    elif cache_filename is not None:
+        with open(cache_filename) as names:
+            kattis_names = set(map(str.strip, names))
+
+    if not kattis_names:
+        error('_general_', 'could not check whether problem names are already used in Kattis')
+        return
+
+    non_unique = sorted(set(problems) & kattis_names)
+
+    if non_unique:
+        error('_general_', f'some problems use names already in Kattis: {non_unique}')
+
+def display_warnings_errors():
+    p = subprocess.run(['git', 'rev-parse', '--short', 'HEAD'], capture_output=True)
+    git_hash = p.stdout.strip().decode('utf-8')
+
+    sep = '-' if git_hash else ''
+    logfile_fmt = f'problem-check-log-%Y%m%d-%H%M%S{sep}{git_hash}.txt'
+    logfilename = datetime.datetime.now().strftime(logfile_fmt)
 
     last_prefix = None
     with open(logfilename, 'w') as out:
@@ -84,15 +127,15 @@ def display_warnings_errors():
             print(x)
             out.write(x + '\n')
 
-        _write('logfile is {}, working directory is {}, git hash is "{}"\n'.format(logfilename, os.getcwd(), git_hash))
+        _write(f'logfile is {logfilename}, working directory is {os.getcwd()}, git hash is "{git_hash}"\n')
 
         for k in sorted(set(_ERRORS.keys())):
             prefix = k.split('/')[0]
             if last_prefix != prefix:
                 last_prefix = prefix
-                _write('\n* {}'.format(prefix))
+                _write(f'\n* {prefix}')
             for e in _ERRORS.get(k, []):
-                _write('    * [ ] {}: {}'.format(k, e))
+                _write(f'    * [ ] {k}: {e}')
 
 ################################################################
 
@@ -110,24 +153,12 @@ def find_problem_directories(wd='.'):
 ################################################################
 # checks involving metadata and names
 
-def _check_problem_name_uniqueness(problems):
-    """Check that all the names are not already used in Kattis."""
-    # TODO: make this not depend on the Kattis database, but perhaps a REST
-    # endpoint
-    with kattis.util.db.db_admin_txn() as conn:
-        cursor = conn.cursor()
-        cursor.execute('SELECT problem_name FROM problem')
-        in_database = {p[0] for p in cursor}
-        non_unique = sorted(set(problems) & in_database)
-
-        if non_unique:
-            error('_general_', 'some problems use names already in Kattis: {}'.format(non_unique))
 
 def _check_problem_name_title(name, title):
     """Check that the problem_name and title seem to match."""
     short_title = re.sub('[^a-zA-Z0-9]', '', title).lower()
     if name != short_title:
-        warning(name, 'use matching directory name and title: {} "{}"'.format(name, title))
+        warning(name, f'use matching directory name and title: {name} "{title}"')
 
 # If people are setting these values, investigate...
 _WARN_ABOUT_SETTINGS = {
@@ -153,14 +184,12 @@ def _check_metadata_recursive(problem, data, default, path=None):
     for k in data:
         full_key = path + ('/' if path else '') + k
         if full_key in _WARN_ABOUT_SETTINGS:
-            warning(problem_yaml, 'specifying unusual metadata value {}'.format(full_key))
+            warning(problem_yaml, f'specifying unusual metadata value {full_key}')
 
         if k not in default:
-            error(problem_yaml, 'option {} is not in default'.format(full_key))
+            error(problem_yaml, f'option {full_key} is not in default')
         elif type(data[k]) == dict:
             _check_metadata_recursive(problem, data[k], default[k], full_key)
-        elif data[k] == default[k]:
-            warning(problem_yaml, 'specifies the default value for {}; remove the definition'.format(full_key))
 
 ################################################################
 # checks involving the problem statement
@@ -174,11 +203,10 @@ def _check_statement(problem):
         (s(r'\\includegraphics(?!\[width=[0-9.]+\\(textwidth|linewidth)\])'), 'bad includegraphics width; use a multiplier (e.g. width=0.9\\textwidth) or HTML layout can break'),
         (s(r'\.\.\.'), 'use \\ldots rather than three periods'),
         (s(r'floating[- ]*point'), 'use "real" rather than "floating-point"'),
-        #(s(r'\bwill\b'), 'uses future tense ("will"); preferably keep language in present tense'),
-        (s(r'\\times\b'), 'use \\cdot instead of \\times'),
+        (s(r'\\times\b'), 'use \\cdot instead of \\times for multiplication'),
     ]
 
-    for filename in glob.glob('{}/problem_statement/problem*.tex'.format(problem)):
+    for filename in glob.glob(os.path.join(problem, 'problem_statement', 'problem*.tex')):
         m = re.match(r'.*problem(?:\.([a-z][a-z]))?.tex$', filename)
         if not m:
             continue
@@ -193,16 +221,22 @@ def _check_statement(problem):
         with open(filename) as filedata:
            lines = filedata.readlines()
            for search, msg in _regex_checks:
-               if any(search(line) for line in lines):
+               if any(map(search, lines)):
                    warning(filename, msg)
 
 
 def _parse_for_tex_errors(problem, filename, spelling_dictionary):
-    """Use the plasTeX parser to iterate over the problem statement and look for issues like:
-        - misspelled words (if spelling_dictionary is not None) 
-        - numbers not in math mode,
+    """Use the plasTeX parser to iterate over the problem statement and look for
+    issues like:
+        - misspelled words (if spelling_dictionary is not None)
+        - numbers not in math mode
         - numbers in math mode not formatted correctly
     """
+
+    if 'plasTeX.TeX' not in sys.modules:
+        error(problem, 'could not load plasTeX for parsing problem description')
+        return
+
     misspelled_words = set()
     incorrect_math = set()
     missing_math_mode = set()
@@ -214,7 +248,6 @@ def _parse_for_tex_errors(problem, filename, spelling_dictionary):
     incorrect_math_re = re.compile(r"\b([0-9]+[0-9,]*,[0-9]+|[0-9]{4,})\b", re.U)
 
     def _check_plain_text(text):
-        #print('_check_plain_text', text)
         words = {m.group(0) for m in plain_text_word_re.finditer(text)}
         if spelling_dictionary:
             for word in words - spelling_dictionary:
@@ -224,7 +257,6 @@ def _parse_for_tex_errors(problem, filename, spelling_dictionary):
                     misspelled_words.add(word)
 
     def _check_math_text(text):
-        #print('_check_math_text', text)
         for word in incorrect_math_re.finditer(text):
             incorrect_math.add(word.group(0))
 
@@ -236,6 +268,7 @@ def _parse_for_tex_errors(problem, filename, spelling_dictionary):
         is_plain_text = isinstance(node, plasTeX.DOM.Text)
         text = node.lower() if is_plain_text else (node.nodeName + ' ')
 
+       # for debugging latex parsing issues
        #print('{indent} {nodeType} {in_math} {is_plain_text} "{nodeRepr}" "{nodeName}"'.format(
        #            indent='  ' * len(path),
        #            nodeType=type(node),
@@ -268,7 +301,7 @@ def _parse_for_tex_errors(problem, filename, spelling_dictionary):
         tex = plasTeX.TeX.TeX(myfile=filename)
         plasTeX.Logging.disableLogging()
         document = tex.parse()
-        print(document)
+        #print(document)
     except Exception as e:
         warning(filename, 'could not parse tex', e)
         return
@@ -281,11 +314,11 @@ def _parse_for_tex_errors(problem, filename, spelling_dictionary):
     _check_math_text(''.join(math_text))
 
     if misspelled_words:
-        warning(filename, 'misspelled words: {}'.format(sorted(misspelled_words)))
+        warning(filename, f'misspelled words: {sorted(misspelled_words)}')
     if incorrect_math:
-        warning(filename, "incorrect math: {} (use `\\,` (backslash comma) to separate thousands groups)".format(sorted(incorrect_math)))
+        warning(filename, f"incorrect math: {sorted(incorrect_math)} (use `\\,` (backslash comma) to separate thousands groups)")
     if missing_math_mode:
-        warning(filename, 'missing math mode: {}'.format(sorted(missing_math_mode)))
+        warning(filename, f'missing math mode: {sorted(missing_math_mode)}')
 
 def _check_metadata(problem):
     """Load the metadata for the given problem and compare its keys and values
@@ -294,9 +327,6 @@ def _check_metadata(problem):
     with open(problem.config.configfile) as f:
         specified_metadata = yaml.safe_load(f)
 
-    print(problem.config)
-    print(problem.config._data)
-    print(full_metadata)
     n = full_metadata['name']
     title = n.get('en', list(n.values())[0])
     _check_problem_name_title(problem.shortname, title)
@@ -310,11 +340,11 @@ def _check_metadata(problem):
 
 def _check_large_images(problem):
     """Warn if there are large images in the problem_statement directory."""
-    for filename in glob.glob('{}/problem_statement/*'.format(problem)):
+    for filename in glob.glob(os.path.join(problem, 'problem_statement', '*')):
         if filename.endswith(('.jpg', '.jpeg', '.png', '.pdf', '.svg')):
             s = os.stat(filename)
             if 1024 * 200 < s.st_size:
-                warning(filename, 'image is large ({} kB) -- try to keep images under 200kB'.format(s.st_size // 1024))
+                warning(filename, f'image is large ({s.st_size // 1024} kB) -- try to keep images under 200kB')
 
 def _check_submissions(problem):
     """Warn if there is not a suitably-robust set of submissions."""
@@ -342,9 +372,14 @@ def _check_submissions(problem):
         if not fast_pattern.match(lang):
             has_slow = True
     if not has_slow:
-        warning(problem.shortname, 'there are no "slow" accepted submissions (only: {})'.format(', '.join(languages_in_accepted)))
+        warning(problem.shortname, f'there are no "slow" accepted submissions (only: {", ".join(languages_in_accepted)})')
 
 def _check_problem(problem):
+
+    if 'problemtools.verifyproblem' not in sys.modules:
+        error(problem, 'could not load problemtools')
+        return {}
+
     with problemtools.verifyproblem.Problem(problem) as p:
         full_metadata = _check_metadata(p)
         _check_submissions(p)
@@ -358,10 +393,10 @@ def _check_problem(problem):
 ################################################################
 # main code for checking everything
 
-def check_problems(problems):
+def check_problems(problems, problem_name_cache=None):
     """Go through the list of problem names and check each one; then compare
     metadata across problems for consistency."""
-    _check_problem_name_uniqueness(problems)
+    _check_problem_name_uniqueness(problems, problem_name_cache)
 
     metadata = {}
     errors = []
@@ -370,41 +405,51 @@ def check_problems(problems):
             metadata[p] = _check_problem(p)
         except:
             errors.append(p)
-            error(p, 'an exception occurred when checking this problem: {}'.format(traceback.format_exc()))
+            error(p, f'an exception occurred when checking this problem: {traceback.format_exc()}')
 
     for k in ['source', 'source_url', 'license']:
-        values = dict(collections.Counter(metadata[p][k] for p in problems if p not in errors))
-        if 1 < len(values):
-            warning('_general_', 'multiple values for {}: {}'.format(k, values))
+        try:
+            values = dict(collections.Counter(metadata[p][k] for p in problems if p not in errors))
+            if 1 < len(values):
+                warning('_general_', f'multiple values for {k}: {values}')
+        except:
+            warning('_general_', f'could not check for consistency of metadata field {k}')
 
-def load_spelling_dictionaries():
+def load_spelling_dictionaries(root):
     global _SPELLING_DICTIONARIES
-    ROOT = '/home/hamerly/etc/dictionaries'
-    for root, dirs, files in os.walk(ROOT, followlinks=True):
-        if root == ROOT:
+    for path, dirs, files in os.walk(root, followlinks=True):
+        if path == root:
             continue
 
-        language = os.path.basename(root)
+        language = os.path.basename(path)
         if language not in _SPELLING_DICTIONARIES:
             _SPELLING_DICTIONARIES[language] = set()
 
         for filename in files:
-            with io.open(os.path.join(root, filename)) as words:
+            with io.open(os.path.join(path, filename)) as words:
                 _SPELLING_DICTIONARIES[language] |= {line.strip().lower() for line in words}
 
-def main():
+            print(f'loaded dictionary {filename} for {language}')
 
-    p = argparse.ArgumentParser()
-    p.add_argument('problems', metavar='problem', nargs='*', default=[])
+def main():
+    p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument('problems', metavar='problem', nargs='*', default=[],
+            help='one or more problems to check (directory names)')
+    p.add_argument('--problem-name-cache',
+            help='file containing a cache of existing problem names used in Kattis, one per line')
+    p.add_argument('--dictionaries',
+            default=os.path.join(pathlib.Path.home(), 'etc', 'dictionaries'),
+            help='directory containing dictionaries for spell checking, one per language')
     args = p.parse_args()
 
-    load_spelling_dictionaries()
+    load_spelling_dictionaries(args.dictionaries)
 
     if args.problems:
         problem_names = args.problems
     else:
         problem_names = find_problem_directories()
-    check_problems(problem_names)
+
+    check_problems(problem_names, args.problem_name_cache)
 
     display_warnings_errors()
 
